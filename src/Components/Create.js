bfa9fb54264fd1d04a05f199b397e0f356b92e06
JericStokMnl <jeric.epon@stok.se>
2021-09-08 16:32:23 +0800

    Full React Tutorial #27 - Controlled Inputs (forms)

--- src/Components/Create.js
+++ src/Components/Create.js
@@ -1,10 +1,73 @@
+import { useState } from "react";
 
 const Create = () => {
+	const [title, setTitle] = useState("");
+	const [body, setBody] = useState("");
+	const [author, setAuthor] = useState("");
+
 	return (
 		<div className="create">
-			<a className="btn btn-primary" href="/#" role="button" onClick={() => {}}>Create a new blog</a>
+			<div className="w-75 mx-auto">
+				<h1 className="mb-4">Add a new blog</h1>
+				<form action="" className="form">
+					<div className="mb-3">
+						<label htmlFor="title" className="form-label">
+							Blog title:
+						</label>
+						<input
+							type="text"
+							name="title"
+							id=""
+							className="form-control"
+							placeholder=""
+							value={title}
+							onChange={(e) => setTitle(e.target.value)}
+						/>
+						{/* <small id="helpId" className="text-muted">Help text</small> */}
+					</div>
+					<div className="mb-3">
+						<label htmlFor="body" className="form-label">
+							Blog body:
+						</label>
+						<textarea
+							className="form-control"
+							name="body"
+							id=""
+							rows="3"
+							onChange={(e) => setBody(e.target.value)}
+							value={body}
+						></textarea>
+						{/* <small id="helpId" className="text-muted">Help text</small> */}
+					</div>
+					<div className="mb-3">
+						<label htmlFor="author" className="form-label">
+							Blog author:
+						</label>
+						<select
+							className="form-select"
+							name="author"
+							onChange={(e) => setAuthor(e.target.value)}
+						>
+							<option defaultValue>Select an author</option>
+							<option value="John">John</option>
+							<option value="Lucas">Lucas</option>
+							<option value="Job">Job</option>
+						</select>
+					</div>
+					<div className="mt-5">
+						<button
+							type="button"
+							name=""
+							id=""
+							className="btn btn-primary"
+						>
+							Add blog
+						</button>
+					</div>
+				</form>
+			</div>
 		</div>
-	)
-}
+	);
+};
 
-export default Create
+export default Create;
