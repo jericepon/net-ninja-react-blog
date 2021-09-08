99726889ee3281a99baefebb5f35f37385cfeccc
JericStokMnl <jeric.epon@stok.se>
2021-09-08 18:37:08 +0800

    Full React Tutorial #32 - 404 Pages & Next Steps

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Create from "./Components/Create";
 
 import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
+import NotFound from "./Components/NotFound";
 
 function App() {
 	return (
@@ -16,6 +17,7 @@ function App() {
 						<Route exact path="/" component={Home}/>
 						<Route path="/create" component={Create}/>
 						<Route path="/blogs/:id" component={BlogDetails}/>
+						<Route path="*" component={NotFound}/>
 					</Switch>
 				</div>
 			</>
